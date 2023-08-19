7aa23682cfef3dfcab7805e8cc627561deb53f60
Humberto Castañeda Garcia <humbertocg@gmail.com>
2023-08-18 18:59:01 -0600

    remove secret key

--- api/api.ts
+++ api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = 'https://api.openweathermap.org';
 export const imgURL = 'https://openweathermap.org/img/w/';
-export const apiKey = '9b534e1208bd95e06bca9a3005729e47';
+export const apiKey = '';
 export enum unitsMeasurements {
   standard = 'standard',
   metric = 'metric',
